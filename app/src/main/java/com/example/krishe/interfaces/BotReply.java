dc17d3b5abf36352e45e3312bab54e0d42153d29
Prateek thakur <prateek.thakur6@gmail.com>
2021-12-09 08:15:06 +0530

    Login register and plant status without api of weather with chatbot not working

---
+++ app/src/main/java/com/example/krishe/interfaces/BotReply.java
@@ -0,0 +1,8 @@
+package com.example.krishe.interfaces;
+
+import com.google.cloud.dialogflow.v2.DetectIntentResponse;
+
+public interface BotReply {
+
+    void callback(DetectIntentResponse returnResponse);
+}
